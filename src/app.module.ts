f88c39183d269ce37a22b38a8b54b01eb28a5e27
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-11-07 01:30:25 -0500

    feat: conexcion a mongo en local

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RubrosModule } from './modules/rubros/rubros.module';
+import { MongooseModule } from '@nestjs/mongoose'
+
 
 @Module({
-  imports: [RubrosModule],
+  imports: [
+    RubrosModule,
+    MongooseModule.forRoot('mongodb://localhost:27018/prueba'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
