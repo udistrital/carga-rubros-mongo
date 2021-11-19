7775db231c417a8f0c0ff2cd41d1887b653dd9c5
Alex Bustos <alexfbp@gmail.com>
2021-11-18 22:39:06 -0500

    fix: docker-compose updated + log CONNECTION_STRING

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RubrosModule } from './modules/rubros/rubros.module';
-import { MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+
+const CONNECTION_STRING = `mongodb://${process.env.USER_MONGO}:${process.env.PASS_MONGO}@${process.env.HOST_MONGO}:${process.env.PORT_MONGO}/${process.env.DB_MONGO}?authSource=admin`;
 
 @Module({
   imports: [
-    ConfigModule.forRoot( { isGlobal: true, } ),
+    ConfigModule.forRoot({ isGlobal: true }),
     RubrosModule,
-    MongooseModule.forRoot(`mongodb://${process.env.USER_MONGO}:${process.env.PASS_MONGO}@${process.env.HOST_MONGO}:${process.env.PORT_MONGO}/${process.env.DB_MONGO}?authSource=admin`),
+    MongooseModule.forRoot(CONNECTION_STRING),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+class AppModule {
+  constructor() {
+    Logger.debug(`CONNECTION_STRING=${CONNECTION_STRING}`);
+  }
+}
+
+export { AppModule };
