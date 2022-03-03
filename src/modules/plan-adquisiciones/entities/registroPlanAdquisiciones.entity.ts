ce0517eed731fcf1029cf4bfa8f99a95a261bebd
brayanpasa99 <brayanpasa99@hotmail.com>
2022-03-02 22:31:37 -0500

    refactor: Sugerencias PR
    Resuelve: udistrital/financiera_documentacion#411

--- src/modules/plan-adquisiciones/entities/registroPlanAdquisiciones.entity.ts
+++ src/modules/plan-adquisiciones/entities/registroPlanAdquisiciones.entity.ts
@@ -90,3 +90,4 @@ export class RegistroPlanAdquisicionesEntity {
       Logger.log(`Se creó la entidad Registro Plan Adquisiciones para el Plan de Adquisiciones: ${this.plan_adquisiciones_id}`);
     }
   }
+  
